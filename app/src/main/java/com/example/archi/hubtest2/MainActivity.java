e3c093b48c3d604a4c8f2a4a1ab311d598f91efc
ArchiZard <42334312+ArchiZard@users.noreply.github.com>
2018-12-18 09:42:44 +0900

    5rd revision in branch1 for push remote new branch

--- app/src/main/java/com/example/archi/hubtest2/MainActivity.java
+++ app/src/main/java/com/example/archi/hubtest2/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
         //3rd revision with creating new branch (프로젝트 폴더 -> git -> repository -> branch에서 만드는  new branch는 local branch임)
         //new branch 만들고 commit은 안함. 그럼 이제 커밋만들면 어케되는지 보자.
         //4th revision VC log에서는 그냥 branch 1에서 commint 만들었다고만 나오네? 이제 commmit 하나 더 만들고 local new branch 에서 push 해볼까?
+        //5th 이번엔 remote branch에 keyin으로 new branch 만들어서 거기다가 push해보자
 
     }
 }
