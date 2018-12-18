5237bed3c585414818e7799b8e19b3d100fec86c
ArchiZard <42334312+ArchiZard@users.noreply.github.com>
2018-12-18 09:15:33 +0900

    1st revision

--- app/src/main/java/com/example/archi/hubtest2/MainActivity.java
+++ app/src/main/java/com/example/archi/hubtest2/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //initial push
+        //1st revision
     }
 }
