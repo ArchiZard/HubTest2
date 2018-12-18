dd79593c1f276c15f5bd7dac64b95adf2a870790
ArchiZard <42334312+ArchiZard@users.noreply.github.com>
2018-12-18 09:33:25 +0900

    3rd revision with creating new branch

--- app/src/main/java/com/example/archi/hubtest2/MainActivity.java
+++ app/src/main/java/com/example/archi/hubtest2/MainActivity.java
@@ -10,7 +10,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //initial push
-        //1st revision
+        // enable
+        //1st revision commit without push
         //2nd revision with push
+        //3rd revision with creating new branch (프로젝트 폴더 -> git -> repository -> branch에서 만드는  new branch는 local branch임)
+        //new branch 만들고 commit은 안함. 그럼 이제 커밋만들면 어케되는지 보자.
+
     }
 }
