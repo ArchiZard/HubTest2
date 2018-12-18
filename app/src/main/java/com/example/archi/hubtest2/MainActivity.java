3d9fd5b22184bfe9848d33f519414ac6ac1e5692
ArchiZard <42334312+ArchiZard@users.noreply.github.com>
2018-12-18 09:16:53 +0900

    2st revision with push

--- app/src/main/java/com/example/archi/hubtest2/MainActivity.java
+++ app/src/main/java/com/example/archi/hubtest2/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //initial push
         //1st revision
+        //2nd revision with push
     }
 }
